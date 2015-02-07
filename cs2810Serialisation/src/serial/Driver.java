1ceb282e6b1a9ddb86f5d1d478425c9930fe2a3b
tirupatihemanth <tirupatihemanthkumar@gmail.com>
2015-02-07 17:04:36 +0000

    lab2_v1

---
+++ cs2810Serialisation/src/serial/Driver.java
@@ -0,0 +1,25 @@
+package serial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <Insert your name>
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Create at least 10 objects of class StudentDetails and set attributes.
+
+		//Add the objects into a list.
+		
+		//Persist the objects to a file using the StudentPersist class.
+		
+		//Retrieve and print them in a sorted order using the Comparable interface in the GetStudentDetails class.
+	}
+
+}
